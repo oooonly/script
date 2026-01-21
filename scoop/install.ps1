ecaf5da809c6a96a4f4a4d9273d3c370e5225657
oooonly <oooohoya@qq.com>
2026-01-21 19:45:32 +0800

    Refactor Scoop installation script with environment variables
    Refactor environment variable setup and installation process for Scoop. Added Deno and PNPM paths.

--- scoop/install.ps1
+++ scoop/install.ps1
@@ -1,9 +1,24 @@
-$env:SCOOP = "C:\applications\Scoop"
-$env:SCOOP_GLOBAL = "C:\applications\Scoop\global"
+# ======================== 1. 路径预设 ========================
+$env:SCOOP        = "C:\applications\Scoop"
+$env:SCOOP_GLOBAL = "$env:SCOOP\global"
+$env:DENO_DIR     = "$env:SCOOP\persist\deno"
+$env:PNPM_HOME    = "$env:SCOOP\persist\pnpm\bin"
+$env:npm_config_store_dir = "$env:SCOOP\persist\pnpm\store"
 
-# 1. 永久设置变量
-[Environment]::SetEnvironmentVariable("SCOOP", $env:SCOOP, "User")
-[Environment]::SetEnvironmentVariable("SCOOP_GLOBAL", $env:SCOOP_GLOBAL, "User")
+# ====================== 2. 设置用户变量 ======================
+Write-Host "--- 正在将配置同步至系统环境变量 ---" -ForegroundColor Cyan
 
-# 2. 安装
+$pathMap = @{
+    "SCOOP"                = $env:SCOOP
+    "SCOOP_GLOBAL"         = $env:SCOOP_GLOBAL
+    "DENO_DIR"             = $env:DENO_DIR
+    "PNPM_HOME"            = $env:PNPM_HOME
+    "npm_config_store_dir" = $env:npm_config_store_dir
+}
+
+foreach ($name in $pathMap.Keys) {
+    [Environment]::SetEnvironmentVariable($name, $pathMap[$name], "User")
+}
+
+# ======================== 3. 安装scoop ========================  
 irm get.scoop.sh | iex
